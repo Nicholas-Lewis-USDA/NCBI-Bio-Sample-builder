7aebf432fb45ea359aa1b6fab5d5479e1ce9fc9f
Nick Lewis <66389049+Nicholas-Lewis-USDA@users.noreply.github.com>
2020-12-02 11:33:27 -0600

    Update NCBI Sample Builder.bat

--- NCBI Sample Builder.bat
+++ NCBI Sample Builder.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 :: This assumes that the "NCBI Bio Sample Builder.ps1" file is located in the user's Downloads folders
-PowerShell -file C:\Users\%USERNAME%\Downloads\\"NCBI Bio Sample Builder.ps1"
+PowerShell -file C:\Users\%USERNAME%\Downloads\\"NCBI Biosample Builder.ps1"
 PAUSE
